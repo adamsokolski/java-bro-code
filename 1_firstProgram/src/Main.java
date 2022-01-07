0c94ea549e927da1d38584e3b85655175e04d013
Adam Sokólski <idKrazu@gmail.com>
2022-01-07 20:09:54 +0100

    ✨ First 2 lessons

---
+++ 1_firstProgram/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I love pizza!\n");
+        System.out.println("It's really good!");
+
+        // yo?
+
+        /*
+        * This
+        * is
+        * a
+        * comment
+        * */
+    }
+}
---
+++ 2_variables/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // initialization
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Adam";
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+        System.out.println("symbol = " + symbol);
+        System.out.println("Hello " + name);
+    }
+}
